ad76409aab5eee3258c5bc1a5a4bbcef23cf8647
alex <alex.palach@gmail.com>
2021-11-21 16:26:41 +0300

    work

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+var str = prompt('Введите своё имя');
+console.log(str);
+alert(str)
+
+
+var age = +prompt('Введите свой возраст');
+console.log(age);
+alert(age)
+
+
+var example = +prompt('15 + 5 = ?');
+alert(example)
+console.log('15 + 5 = 20 ' + ' Ваш ответ: ' + example);
+
+var example_2 = +prompt('10 - 3 = ?');
+alert(example_2);
+console.log('10 - 3 = 7 ' + 'Ваш ответ: ' + example_2);
+
+var example_3 = +prompt('2 * 5 = ?');
+alert(example_3);
+console.log('2 * 5 = 10 ' + 'Ваш ответ: ' + example_3);
+
+var example_4 = +prompt('20 / 4 = ?');
+alert(example_4);
+console.log('20 / 4 = 5 ' + 'Ваш ответ: ' + example_4);
+
+var example_5 = +prompt('6 % 5= ?');
+alert(example_5);
+console.log('6 % 5 = 1 ' + 'Ваш ответ: ' + example_5);
+
+var way = prompt('Нажмите Enter что бы перейти в консоль')
